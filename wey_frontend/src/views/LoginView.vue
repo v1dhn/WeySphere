b48ec02b16f833fa8daa8f6fed92a788bb772795
vidhan <vidhn@hotmail.com>
2024-03-03 18:29:16 +0530

    static signup and login page

--- wey_frontend/src/views/LoginView.vue
+++ wey_frontend/src/views/LoginView.vue
@@ -2,23 +2,19 @@
     <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
         <div class="main-left">
             <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <h1 class="mb-6 text-2xl">Sign up</h1>
+                <h1 class="mb-6 text-2xl">Login</h1>
                 <p class="mb-6 text-gray-500">
                             Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate.
                             Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate.
                 </p>
                 <p class="font-bold">
-                            Already have an account? <a href="#" class="underline">Click here</a> to log in!
+                            Don't have an account? <a href="#" class="underline">Click here</a> to create one!
                         </p>
             </div>
         </div>
         <div class="main-right">
             <div class="p-12 bg-white border border-gray-200 rounded-lg">
                 <form class="space-y-6">
-                            <div>
-                                <label>Name</label><br>
-                                <input type="name" placeholder="Full Name" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                            </div>
                             
                             <div>
                                 <label>E-mail</label><br>
@@ -31,12 +27,7 @@
                             </div>
 
                             <div>
-                                <label>Repeat password</label><br>
-                                <input type="password" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                            </div>
-
-                            <div>
-                                <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Sign up</button>
+                                <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Log in</button>
                             </div>
                         </form>
             </div>
