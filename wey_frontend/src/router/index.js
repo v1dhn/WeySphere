c44d3518d98508167464cc7e100f947d65866a19
vidhan <vidhn@hotmail.com>
2024-03-10 22:03:12 +0530

    static page templates

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignupView from '../views/SignupView.vue'
 import LoginView from '../views/LoginView.vue'
-
+import FeedView from '../views/FeedView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +11,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/feed',
+      name: 'feed',
+      component: FeedView
     },
     {
       path: '/signup',
